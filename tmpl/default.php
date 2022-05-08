c138dedf22a8db98918934624f2a52cb5c923ffd
KoenVDZ <koenatwork@gmail.com>
2022-05-08 21:21:05 +0200

    Update WhatsApp Number

--- tmpl/default.php
+++ tmpl/default.php
@@ -23,7 +23,7 @@ $lang->load('mod_mscontactbtns', JPATH_SITE);
     <span class="contact_button social_icon_es"><i class="far fa-envelope"></i><?php echo Text::_('MOD_MSCONTACTBTNS_CORREO_ELEC'); ?></span>
   </div>
   <div class="contact_button-item">
-    <a class="contact_button-anchor" href="https://api.whatsapp.com/send?phone=5491157548291" target="_blank" rel="noopener noreferrer">
+    <a class="contact_button-anchor" href="https://api.whatsapp.com/send?phone=32456340498" target="_blank" rel="noopener noreferrer">
       <span class="contact_button"><i class="fab fa-whatsapp"></i><?php echo Text::_('MOD_MSCONTACTBTNS_WHATSAPP'); ?></span>
     </a>
   </div>
